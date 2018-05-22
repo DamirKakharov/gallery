bebf7cbe1bc753a9ff557a4159fb06c29bc465a0
DamirKakharov <k.damir97@gmail.com>
2018-05-22 16:01:16 +0600

    Добавил стили для компонента search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,47 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './search.scss';
 
 class Search extends React.Component {
     static propTypes = {
-        updateData: PropTypes.func.isRequired,
+        respSearch: PropTypes.func.isRequired,
+        setSearch: PropTypes.func.isRequired,
     };
 
-    constructor() {
-        super();
-        this.state = {
-            name: '',
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.onKeyPressed = this.onKeyPressed.bind(this);
-    }
-
-    // onKeyPressed(e) {
-    //     if (e.keyCode === 13) {
-    //         this.props.updateData(this.state.name);
-    //         console.log('df');
-    //     }
-    // }
-
-    onKeyPressed(e) {
-        if (e.keyCode === 13) {
-            return this.props.setSearch(this.state.name);
-            console.log(this.state.name);
+    onKeyPressed = (event) => {
+        if (event.keyCode === 13) {
+            this.props.respSearch();
         }
-        return e;
     }
 
-    handleChange(event) {
-        this.setState({ name: event.target.value });
+    handleClick = () => {
+        this.props.respSearch();
+    }
+
+    handleChange = (event) => {
+        this.props.setSearch(event.target.value);
     }
 
     render() {
         return (
-            <div>
-                {/* <input tabIndex="0" type="text" value={this.state.value} onChange={this.handleChange} onKeyDown={this.onKeyPressed} /> */}
-                <input tabIndex="0" type="text" value={this.state.value} onChange={this.handleChange} onKeyDown={this.onKeyPressed} />
-                <button onClick={() => { this.props.updateData(this.state.name); }}>Search</button>
+            <div className="search">
+                <input className="search__input" tabIndex="0" type="text" onChange={this.handleChange} onKeyDown={this.onKeyPressed} />
+                <button className="search__button" onClick={this.handleClick}>Search</button>
             </div>
         );
     }
