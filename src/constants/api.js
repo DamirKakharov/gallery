236952d25ef2b4731f08030812276cc6424fdcd4
DamirKakharov <k.damir97@gmail.com>
2018-05-22 16:05:49 +0600

    Добавил константы запросов

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,19 @@
-export const PATH = 'https://api.unsplash.com/photos';
+export const PATH = 'https://api.unsplash.com/photos?client_id=';
+export const PATH_SEARCH = 'https://api.unsplash.com/search/photos?client_id=';
+
+export const PAGE = '&page=';
+export const PER_PAGE = '&per_page=';
+
+export const QUERY = '&query=';
+
 export const KEY = 'bcda5d758ceb319f79bd6eeff203a979b75cd35015bf422f5a0877b4ab03fa7a';
-export const PER_PAGE = 10;
+
+export const masonryOptions = {
+    transitionDuration: '0.5s',
+    // columnWidth: 300,
+    resize: true,
+    gutter: 20,
+    percentPosition: true,
+    fitWidth: true,
+};
 
