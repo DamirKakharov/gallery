7e72a8cb2f8c668a280f5d16214518ceb212c51f
DamirKakharov <k.damir97@gmail.com>
2018-05-24 10:13:14 +0600

    Добавил Reduseк

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import searchResponse from './search.reducer';
-
+import homePageResponse from './home.reduser';
 
 const rootReducer = combineReducers({
     searchResponse,
+    homePageResponse,
 });
 
 export default rootReducer;
--- src/reducers/search.reducer.js
+++ src/reducers/search.reducer.js
@@ -1,10 +1,12 @@
+import { SET_SEARCH } from '../actions/search.action';
+
 const initialState = {
     search: '',
 };
 
 export default function searchResponse(state = initialState, action) {
     switch (action.type) {
-    case 'SET_SEARCH':
+    case SET_SEARCH:
         return { ...state, search: action.payload };
     default:
         return state;
