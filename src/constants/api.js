cc7dbd60d60e1d3054b68165b662062ab4507ac8
DamirKakharov <k.damir97@gmail.com>
2018-05-24 10:15:48 +0600

    Вынес ненужные const

--- src/constants/api.js
+++ src/constants/api.js
@@ -7,13 +7,3 @@ export const PER_PAGE = '&per_page=';
 export const QUERY = '&query=';
 
 export const KEY = 'bcda5d758ceb319f79bd6eeff203a979b75cd35015bf422f5a0877b4ab03fa7a';
-
-export const masonryOptions = {
-    transitionDuration: '0.5s',
-    // columnWidth: 300,
-    resize: true,
-    gutter: 20,
-    percentPosition: true,
-    fitWidth: true,
-};
-
