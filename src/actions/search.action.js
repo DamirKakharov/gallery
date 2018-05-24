46326b4e9a5979443054605b60dd51928ca92ca8
DamirKakharov <k.damir97@gmail.com>
2018-05-24 10:06:59 +0600

    Добавио новые Actions

--- src/actions/search.action.js
+++ src/actions/search.action.js
@@ -1,6 +1,38 @@
-export default function setSearchAction(search) {
+import Api from '../api/api';
+
+export const SET_SEARCH = 'SET_SEARCH';
+export const CHANGE_RAGE = 'CHANGE_PAGE';
+
+export function setSearchAction(search) {
     return {
-        type: 'SET_SEARCH',
+        type: SET_SEARCH,
         payload: search,
+    };
+}
+
+export function changePage(findResponse) {
+    return {
+        type: CHANGE_RAGE,
+        payload: findResponse,
+    };
+}
+
+export function loadHomePage() {
+    return function (dispatch) {
+        return Api.getHomePage().then((findResponse) => {
+            dispatch(changePage(findResponse));
+        }).catch((error) => {
+            throw (error);
+        });
+    };
+}
+
+export function loadSearchPage(searchResp) {
+    return function (dispatch) {
+        return Api.getSearchPage(searchResp).then((findResponse) => {
+            dispatch(changePage(findResponse.results));
+        }).catch((error) => {
+            throw (error);
+        });
     };
 }
